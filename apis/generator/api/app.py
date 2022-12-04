8b43ab8fb18ea372e9e65df756535d3e1d0d2dc5
Kolja <kolja.kieslich@gmail.com>
2022-12-04 20:29:22 +0100

    prepared kuka model integration

--- apis/generator/api/app.py
+++ apis/generator/api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -7,3 +8,13 @@ CORS(app)
 @app.route("/")
 def home():
     return "Generator api"
+
+@app.route("/urdf")
+def get_urdfs():
+    urdfs_dir = "urdfs"
+    modelNames = list(name for name in os.listdir(urdfs_dir) if os.path.isdir(f"{urdfs_dir}/{name}"))
+    return modelNames
+
+if __name__ == "__main__":
+    # predict(model="base_model", dataset_name="test_files", input_data="br")
+    app.run(debug=True, host="0.0.0.0", port=5003, threaded=False, processes=8)
