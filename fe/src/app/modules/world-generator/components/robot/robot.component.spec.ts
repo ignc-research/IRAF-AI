5669f61cb4c33745dfc3cb35135ba63f92d0c53a
kupkovic <roman@turboad.de>
2022-12-19 17:31:31 +0100

    typo

--- fe/src/app/modules/world-generator/components/robot/robot.component.spec.ts
+++ fe/src/app/modules/world-generator/components/robot/robot.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RobotComponent } from './robot.component';
 
-describe('CubeComponent', () => {
+describe('RobotComponent', () => {
   let component: RobotComponent;
   let fixture: ComponentFixture<RobotComponent>;
 
